611325b248c8fe00d4ccb30b2ea209b1dea6c050
sarashs <sarashs@DESKTOP-AI964DL.localdomain>
2020-06-01 14:05:00 -0700

    Input and atom file creators are moved to the LAMMPS_Utils.py and the script folder was removed.

--- LAMMPS_Utils.py
+++ LAMMPS_Utils.py
@@ -5,12 +5,14 @@ Created on Fri May 29 12:33:21 2020
 
 @author: sarashs
 LAMMPS utils containing:
-    lammps_input_creator
+    lammps_input_creator : Which prepares the lammps inputfile.dat 
+    geofilecreator : Prepares lammps atom files [also known as geometry file] atom.data 
 
 """
+import re
 def lammps_input_creator(Input_structure_file="Inputstructurefile.txt",Input_forcefield='ffield.reax',Forcefield_type = 'reax'):
     """
-    This file creates the lammps inputs
+    This function creates the lammps input file
     :param Input_structure_file:
     :param Input_forcefield:
     :return: Input_data_file_list
@@ -93,5 +95,57 @@ def lammps_input_creator(Input_structure_file="Inputstructurefile.txt",Input_for
               s.write('min_style cg\n')
               s.write('minimize 1.0e-7 1.0e-9 20000 20000\n')
               s.write('undump DUMP2\n')
+           s.close()
+    f.close()
+
+def geofilecreator(Input_structure_file="Inputstructurefile.txt"):
+    """
+    This function creates lammps data files
+    """
+    f=open(Input_structure_file,'r')
+    l=f.readlines()
+    for item in l:
+           atom_type=0
+           if '#structure ' in item:
+              LAMMPS_Data_file=l[l.index(item)].replace('#structure ','').replace('\n','').replace(' ','')+".data"
+              s=open(LAMMPS_Data_file,'w')
+              s.close()
+              s=open(LAMMPS_Data_file,'a')
+              #for n in lists:
+              s.write('# System description #######################\n')
+              s.write('#\n')
+              s.write('\n')
+              #line for number of atoms
+              s.write(l[l.index(item)+1].replace('\n','  atoms\n')) 
+              number_of_atoms=int(l[l.index(item)+1])
+              #line for atom types
+              for item2 in l[(l.index(item)+3):]:
+                 if not ('#dimensions' in item2):
+                    atom_type=atom_type+1
+                 else:
+                    break   
+              s.write('%d atom types\n' % atom_type)
+              #line for box dimentions
+              dimensions=re.findall(r"[-+]?\d*\.\d+|\d+",l[(l.index(item)+4+atom_type)])
+              #s.write(l[(l.index(item)+3+atom_type)])
+              s.write('0 %f xlo xhi\n' % float(dimensions[0]) )
+              s.write('0 %f ylo yhi\n' % float(dimensions[1]) )
+              s.write('0 %f zlo zhi\n' % float(dimensions[2]) )
+              s.write('#\n')
+              s.write('# for a crystal:\n')
+              s.write('# lx=a;  ly2+xy2=b2;  lz2+xz2+yz2=c2\n')
+              s.write('# xz=c*cos(beta);  xy=b*cos(gamma)\n')
+              s.write('# xy*xz+ly*yz=b*c*cos(alpha)\n')
+              s.write('#\n\n')
+              s.write('# Elements #################################\n\n')
+              s.write('Masses\n\n')
+              # line for atomic masses
+              for i in range(1,atom_type+1):
+                 s.write(l[l.index(item)+2+i].replace(l[l.index(item)+2+i][0:2],'%d ' % i))
+              s.write('\nAtoms\n')
+              for item2 in l[(l.index(item)+5+atom_type):(l.index(item)+5+atom_type+number_of_atoms)]:
+                 for i in range(1,atom_type+1):
+                        item2=item2.replace(l[l.index(item)+2+i][0:2],'%d ' % i)
+                 s.write('\n'+item2.replace('\n',''))
            s.close()
     f.close()
