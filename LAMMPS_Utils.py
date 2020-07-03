f18b47ee1da2d0d9d60db0183b8638e70fb9a636
sarashs <sarashs@DESKTOP-AI964DL.localdomain>
2020-07-02 17:57:59 -0700

    A new function was added to LAMMPS Util

--- LAMMPS_Utils.py
+++ LAMMPS_Utils.py
@@ -5,11 +5,36 @@ Created on Fri May 29 12:33:21 2020
 
 @author: sarashs
 LAMMPS utils containing:
+    
+    Parameters:
+    atomic_weight_dict
+    
+    Functions:
+        
     lammps_input_creator : Which prepares the lammps inputfile.dat 
     geofilecreator : Prepares lammps atom files [also known as geometry file] atom.data 
-
+    append_2structure_file : Avogadro xyz to input structure file
 """
 import re
+
+atomic_weight_dict = {"H" : 1.0079, "He" : 4.0026,"Li" : 6.941,"Be" : 9.0122,\
+                      "B" : 10.811, "C" : 12.0107,"N" : 14.0067,"O" : 15.9994,\
+                      "F" : 18.9984, "Ne" : 20.1797, "Na" : 22.9897, "Mg" : 24.305,\
+                      "Al" : 26.9815, "Si" : 28.0855, "P" : 30.9738, "S" : 32.065,\
+                      "Cl" : 35.453, "K" : 39.0983, "Ar" : 39.948, "Ca" : 40.078,\
+                      "Sc" : 44.9559, "Ti" : 47.867, "V" : 50.9415, "Cr" : 51.9961,\
+                      "Mn" : 54.938, "Fe" : 55.845, "Ni" : 58.6934, "Co" : 58.9332,\
+                      "Cu" : 63.546, "Zn" : 65.39, "Ga" : 69.723, "Ge" : 72.64,\
+                      "As" : 74.9216, "Se" : 78.96, "Br" : 79.904, "Kr" : 83.8,\
+                      "Rb" : 85.4678, "Sr" : 87.62, "Y" : 88.9059, "Zr" : 91.224,\
+                      "Nb" : 92.9064, "Mo" : 95.94, "Tc" : 98.00, "Ru" : 101.07,\
+                      "Rh" : 102.9055, "Pd" : 106.42, "Ag" : 107.8682, "Cd" : 112.411,\
+                      "In" : 114.818, "Sn" : 118.71, "Sb" : 121.76, "I" : 126.9045,\
+                      "Te" : 127.6, "Xe" : 131.293, "Cs" : 132.9055, "Ba" : 137.327,\
+                      "La" : 138.9055, "Hf" : 178.49, "Ta" : 180.9479, "W" : 183.84,\
+                      "Pt" : 195.078, "Au" : 196.9665, "Hg" : 200.59, "Pb" : 207.2,\
+                      "Bi" : 208.9804, "U" : 238.0289}
+
 def lammps_input_creator(Input_structure_file="Inputstructurefile.txt",Input_forcefield='ffield.reax',Forcefield_type = 'reax', file_path = ""):
     """
     This function creates the lammps input file
@@ -150,3 +175,41 @@ def geofilecreator(Input_structure_file="Inputstructurefile.txt", file_path = ""
                  s.write('\n'+item2.replace('\n',''))
            s.close()
     f.close()
+
+def append_2structure_file(input_files_path, output_files_path, input_xyz_name, structure_file_name, box_dim = [100, 100, 100]):
+    """This function reformats and appends a given XYZ file to the input structure file. 
+    This function does not add the bond retraints etc.
+    :param input_files_path:
+    :param output_file_path:
+    :param xyz input file name:
+    :param structure file name:
+    :param box_dim:[x y z]
+    :return: NULL"""
+    if ".xyz" in input_xyz_name[-4:]:
+        input_xyz_name = input_xyz_name[:-4]
+    if ".txt" in structure_file_name[-4:]:
+        structure_file_name = structure_file_name[:-4]
+    try:
+        f=open(input_files_path + input_xyz_name + ".xyz",'U')
+        l=f.readlines()
+        f.close()
+    except IOError:
+        print('An error occured trying to read the xyz file.')
+    num_atoms = int(l[0].replace('\n','').replace(' ',''))
+    set_of_atoms = set([])
+    for item in l[2:(2+num_atoms)]:
+        set_of_atoms.add(item[:3].replace(' ',''))
+        
+    try:
+        S=open(output_files_path + structure_file_name + ".txt",'a')
+    except IOError:
+        print('The structure (output) file cannot be opened.')
+    S.write('#structure ' + input_xyz_name + "\n")
+    S.write(l[0])
+    S.write('#weights' + "\n")
+    for item in set_of_atoms:
+        S.write(item + " " + str(atomic_weight_dict[item]) + "\n")
+    S.write("#dimensions\n")
+    S.write(str(box_dim[0]) + " " + str(box_dim[1]) + " " + str(box_dim[2]) + "\n")
+    S.writelines(l[2:(2+num_atoms)])
+    S.close()
