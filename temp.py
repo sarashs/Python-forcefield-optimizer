5b86da837f825f1a135b96793131787b2d1d9aeb
sarashs <sarashs@DESKTOP-AI964DL.localdomain>
2020-07-16 21:33:31 -0700

    new simulation

--- temp.py
+++ temp.py
@@ -14,18 +14,19 @@ from LAMMPS_Utils import lammps_input_creator, geofilecreator, append_2structure
 import pylab
 #from mpi4py import MPI
 
+#For ZrOSi
+ff_filePath = '/home/sarashs/Python-forcefield-optimizer/tests/Zr_Si_forcefield/ffield1.reax'
+ParamSelect_filePath = '/home/sarashs/Python-forcefield-optimizer/tests/Zr_Si_forcefield/params'
+Training_file = 'Trainingfile.txt'
+Input_structure_file = '/home/sarashs/Python-forcefield-optimizer/tests/Zr_Si_forcefield/Zr_O_Si_structure.txt'
+output_path = '/home/sarashs/Python-forcefield-optimizer/tests/Zr_Si_forcefield/'
+geofilecreator(Input_structure_file, output_path)
+a = SA_REAX_FF(ff_filePath, output_path, ParamSelect_filePath, Training_file, Input_structure_file, T=100, T_min=10, Temperature_decreasing_factor=0.1, max_iter=15, number_of_points=1)
+a.anneal(record_costs = "YES", repelling_weight = 1)
+listaa = [item['annealer_0.reax'] for item in a.costs]
+pylab.plot(listaa)
 
-#ff_filePath = '/home/sarashs/Python-forcefield-optimizer/tests/Zr_Si_forcefield/ffield1.reax'
-#ParamSelect_filePath = '/home/sarashs/Python-forcefield-optimizer/tests/Zr_Si_forcefield/params'
-#Training_file = 'Trainingfile.txt'
-#Input_structure_file = '/home/sarashs/Python-forcefield-optimizer/tests/Zr_Si_forcefield/Zr_O_Si_structure.txt'
-#output_path = '/home/sarashs/Python-forcefield-optimizer/tests/Zr_Si_forcefield/'
-#geofilecreator(Input_structure_file, output_path)
-#a = SA_REAX_FF(ff_filePath, output_path, ParamSelect_filePath, Training_file, Input_structure_file, T=10, T_min=1, Temperature_decreasing_factor=0.1, max_iter=10, number_of_points=1)
-#a.anneal(record_costs = "YES", repelling_weight = 1)
-#listaa = [item['annealer_0.reax'] for item in a.costs]
-#pylab.plot(listaa)
-
+##For CL
 #ff_filePath = '/home/sarashs/Python-forcefield-optimizer/tests/ffieldoriginal.txt'
 #ParamSelect_filePath = '/home/sarashs/Python-forcefield-optimizer/tests/params'
 #Training_file = 'Trainingfile_2.txt'
@@ -41,14 +42,14 @@ import pylab
 #pylab.plot(listab)
 
 ##create the input_structure_file
-import os
-for file_name in os.listdir("/home/sarashs/Python-forcefield-optimizer/tests/Zr_Si_forcefield/OH3_Si_O_Zr_OH3_B3LYP/"):
-    if file_name.endswith(".xyz"):
-        append_2structure_file("/home/sarashs/Python-forcefield-optimizer/tests/Zr_Si_forcefield/OH3_Si_O_Zr_OH3_B3LYP/", "/home/sarashs/Python-forcefield-optimizer/tests/", file_name, "Zr_O_Si_structure", box_dim = [100, 100, 100],  restrain = "angle 1 5 6 2000 2000\n")
+#import os
+#for file_name in os.listdir("/home/sarashs/Python-forcefield-optimizer/tests/Zr_Si_forcefield/OH3_Si_O_Zr_OH3_B3LYP/"):
+#    if file_name.endswith(".xyz"):
+#        append_2structure_file("/home/sarashs/Python-forcefield-optimizer/tests/Zr_Si_forcefield/OH3_Si_O_Zr_OH3_B3LYP/", "/home/sarashs/Python-forcefield-optimizer/tests/", file_name, "Zr_O_Si_structure", box_dim = [100, 100, 100],  restrain = "angle 1 5 6 2000 2000\n")
 
   
 ###extract the energies     
 #import os
-#for file_name in os.listdir("/home/sarashs/Python-forcefield-optimizer/tests/Zr_Si_forcefield/equilibriums/"):
+#for file_name in os.listdir("/home/sarashs/Python-forcefield-optimizer/tests/Zr_Si_forcefield/OH3_Si_O_Zr_OH3_B3LYP/"):
 #    if file_name.endswith(".log"):
-#        gaussian_energy_extractor("/home/sarashs/Python-forcefield-optimizer/tests/Zr_Si_forcefield/equilibriums/", "/home/sarashs/Python-forcefield-optimizer/tests/", file_name, "gaussian_energies")
+#        gaussian_energy_extractor("/home/sarashs/Python-forcefield-optimizer/tests/Zr_Si_forcefield/OH3_Si_O_Zr_OH3_B3LYP/", "/home/sarashs/Python-forcefield-optimizer/tests/", file_name, "gaussian_energies")
